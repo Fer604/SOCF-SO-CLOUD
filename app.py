5094c33bceeb94807b24a74a41fa0af09925bef3
andrééé <andreestevesarantes19@gmail.com>
2025-11-10 20:22:42 -0300

    feat: new route

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ APP = Flask(__name__)
 def index():
     return "<h1>cole familia</h1>"
 
+@APP.get("/info")
+def info():
+    return "Integrantes: \nAndré Esteves Arantes \nFernando Aschwanden \nGustavo Jansen"
 
 if __name__ == '__main__':
     APP.run(host='0.0.0.0',port=80)
