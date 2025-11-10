7a7242d3afdb31f271f6870fa5c9dabdf820d70e
Fer604 <166722929+Fer604@users.noreply.github.com>
2025-11-10 21:50:02 +0000

    primeiro d vdd

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+APP = Flask(__name__)
+
+
+@APP.get("/")
+def index():
+    return "<h1>cole familia</h1>"
+
+
+if __name__ == '__main__':
+    APP.run(host='0.0.0.0',port=80)
